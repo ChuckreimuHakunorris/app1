990016935c595538e4bbda03e13cc64ccb34a29d
Ville Laaksonen <ville.laaksonen@wideflow.ai>
2024-05-31 13:18:06 +0300

    added header component and related styles and html files

--- src/app/header.component.ts
+++ src/app/header.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-header',
-  template: '',
+  standalone: true,
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.css',
 })
 export class HeaderComponent {}
