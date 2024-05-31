e318c4063d40e597b1b318f15d84199044055ed6
Ville Laaksonen <ville.laaksonen@wideflow.ai>
2024-05-31 14:59:33 +0300

    created user component, added dummy user data and images

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
-import { HeaderComponent } from './header.component';
+import { HeaderComponent } from './header/header.component';
+import { UserComponent } from './user/user.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
+  imports: [RouterOutlet, HeaderComponent, UserComponent],
 })
 export class AppComponent {
   title = 'app1';
